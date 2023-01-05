8b7e8880f0122655c11f3dcacfe9e1be083bbc38
maxtan84 <mthockey84@gmail.com>
2023-01-04 21:50:59 -0500

    dark mode btn added, need to add colours

--- js/index.js
+++ js/index.js
@@ -1,8 +1,13 @@
 const navToggle = document.querySelector('.nav-toggle');
-const navLinks = document.querySelectorAll('.nav__link')
+const navLinks = document.querySelectorAll('.nav__link');
+const darkModeBtn = document.querySelector('.dark-mode-btn');
 
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
+});
+
+darkModeBtn.addEventListener('click', () => {
+    document.body.classList.toggle('dark');
 });
 
 navLinks.forEach(link => {
